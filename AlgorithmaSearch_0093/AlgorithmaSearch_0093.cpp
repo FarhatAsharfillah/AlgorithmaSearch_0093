1b93be78124aa209cefcfa7bb169567444af35b2
FarhatAsharfillah <farhatasharfillah03@gmail.com>
2024-04-05 14:56:44 +0700

    commit 4 input "masukan bilangan elemen dalam array

--- AlgorithmaSearch_0093/AlgorithmaSearch_0093.cpp
+++ AlgorithmaSearch_0093/AlgorithmaSearch_0093.cpp
@@ -4,3 +4,11 @@ using namespace std;
 int arr[20]; //	Array untuk dicari
 int n;		 // Bilangan elemen dalam array
 int i;		 // Index elemen
+
+void input()
+{
+	while (true)
+	{
+		cout << "Masukan bilangan elemen dalam array"
+	}
+}
